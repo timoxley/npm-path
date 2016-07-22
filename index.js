081ce8eaf17e79dffa278af84364ccf1607e71f7
Tim Oxley <secoif@gmail.com>
2016-07-22 12:58:52 +0800

    Conform to standard style.

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-"use strict"
+'use strict'
 
-var fs   = require('fs')
-var path = require('path')
+const fs = require('fs')
+const path = require('path')
+const which = require('which')
 
-var which = require('which')
+const findPrefix = require('./find-prefix')
 
-var findPrefix = require('./find-prefix')
-
-
-var PATH      = getPATHKey()
-var SEPARATOR = getPATHSeparator()
-
+const PATH = getPATHKey()
+const SEPARATOR = getPATHSeparator()
 
 /**
  * Get new $PATH setting with additional paths supplied by the npm.
@@ -20,22 +17,23 @@ var SEPARATOR = getPATHSeparator()
  * @param String options.wd Working directory. Default: process.cwd()
  * @param Function fn callback function.
  */
-function getPath(options, fn) {
-  var wd = options.cwd = options.cwd || process.cwd()
-  var env = options.env = options.env || process.env
-  var pathArr = getPathArr(options)
 
-  whichNpm(options, function(err, npmPath) {
+function getPath (options, fn) {
+  options.cwd = options.cwd || process.cwd()
+  const env = options.env = options.env || process.env
+  let pathArr = getPathArr(options)
+
+  whichNpm(options, function (err, npmPath) {
     if (err) return fn(err)
     findPrefix(options, function (err, prefixPath) {
       if (!err && prefixPath) {
         // ignore err if cannot find prefix
-        pathArr.unshift(path.join(prefixPath, "node_modules", ".bin"))
+        pathArr.unshift(path.join(prefixPath, 'node_modules', '.bin'))
       }
 
       // we also unshift the bundled node-gyp-bin folder so that
       // the bundled one will be used for installing things.
-      pathArr.unshift(path.join(path.dirname(npmPath), "node-gyp-bin"))
+      pathArr.unshift(path.join(path.dirname(npmPath), 'node-gyp-bin'))
       if (env[PATH]) pathArr = pathArr.concat(env[PATH].split(SEPARATOR))
 
       // Remove duplicated entries
@@ -49,9 +47,13 @@ function getPath(options, fn) {
 /**
  * Async wrapper around `getPath`.
  */
-function getPathAsync(options, fn) {
+
+function getPathAsync (options, fn) {
   // options is optional
-  if (options instanceof Function) fn = options, options = {}
+  if (options instanceof Function) {
+    fn = options
+    options = {}
+  }
   // if no fn, execute as sync
   if (!(fn instanceof Function)) return getPathSync(options)
   options = options || {}
@@ -62,13 +64,14 @@ function getPathAsync(options, fn) {
 /**
  * Sync wrapper around `getPath`.
  */
-function getPathSync(options) {
+
+function getPathSync (options) {
   options = options || {}
   options.isSync = true
-  var thePath = undefined
+  let thePath = null
   // sync magic: if sync true, callback is executed sync
   // therefore we can set thePath from inside it before returning
-  getPath(options, function(err, foundPath) {
+  getPath(options, function (err, foundPath) {
     if (err) throw err
     thePath = foundPath
   })
@@ -83,13 +86,18 @@ function getPathSync(options) {
  * @param String options.wd Working directory. Default: process.cwd()
  * @param Function fn callback function.
  */
-function setPathAsync(options, fn) {
+
+function setPathAsync (options, fn) {
   // options is optional
-  if (options instanceof Function) fn = options, options = {}
+  if (options instanceof Function) {
+    fn = options
+    options = {}
+  }
+
   // if no fn, execute as sync
   if (!(fn instanceof Function)) return setPathSync(options)
 
-  getPathAsync(options, function(err, newPath) {
+  getPathAsync(options, function (err, newPath) {
     if (err) return fn(err)
     fn(null, options.env[PATH] = newPath)
   })
@@ -98,10 +106,11 @@ function setPathAsync(options, fn) {
 /**
  * Sync version of `setPathAsync`
  */
-function setPathSync(options) {
+function setPathSync (options) {
   options = options || {}
-  var newPath = getPathSync(options)
-  return options.env[PATH] = newPath
+  const newPath = getPathSync(options)
+  options.env[PATH] = newPath
+  return newPath
 }
 
 /**
@@ -110,22 +119,22 @@ function setPathSync(options) {
  *
  * @return Array
  */
-function getPathArr(options) {
-  var wd = options.cwd
-  var pathArr = []
-  var p = wd.split(path.sep + "node_modules" + path.sep)
-  var acc = path.resolve(p.shift())
+function getPathArr (options) {
+  const wd = options.cwd
+  const pathArr = []
+  const p = wd.split(path.sep + 'node_modules' + path.sep)
+  let acc = path.resolve(p.shift())
 
   // first add the directory containing the `node` executable currently
-  // running, so that any lifecycle script that invoke "node" will execute
+  // running, so that any lifecycle script that invoke 'node' will execute
   // this same one.
   pathArr.unshift(path.dirname(process.execPath))
 
   p.forEach(function (pp) {
-    pathArr.unshift(path.join(acc, "node_modules", ".bin"))
-    acc = path.join(acc, "node_modules", pp)
+    pathArr.unshift(path.join(acc, 'node_modules', '.bin'))
+    acc = path.join(acc, 'node_modules', pp)
   })
-  pathArr.unshift(path.join(acc, "node_modules", ".bin"))
+  pathArr.unshift(path.join(acc, 'node_modules', '.bin'))
   return pathArr
 }
 
@@ -133,8 +142,8 @@ function getPathArr(options) {
  * Use callback-style signature but toggle sync execution if `isSync` is true.
  * If options.npm is supplied, this will simply provide npm/bin/npm-cli.
  */
-function whichNpm(options, fn) {
-  var npmCli = options.npm && path.join(options.npm, 'bin', 'npm-cli.js')
+function whichNpm (options, fn) {
+  const npmCli = options.npm && path.join(options.npm, 'bin', 'npm-cli.js')
 
   if (options.isSync) {
     fn(null, fs.realpathSync(
@@ -144,13 +153,13 @@ function whichNpm(options, fn) {
   }
 
   if (options.npm) {
-    process.nextTick(function() {
+    process.nextTick(function () {
       fn(null, npmCli)
     })
     return
   }
 
-  which('npm', function(err, npmPath) {
+  which('npm', function (err, npmPath) {
     if (err) return fn(err)
     fs.realpath(npmPath, fn)
   })
@@ -159,12 +168,13 @@ function whichNpm(options, fn) {
 /**
  * Get key to use as $PATH in environment
  */
-function getPATHKey() {
-  var PATH = 'PATH'
 
-  // windows calls it's path "Path" usually, but this is not guaranteed.
-  if (process.platform === "win32") {
-    PATH = "Path"
+function getPATHKey () {
+  let PATH = 'PATH'
+
+  // windows calls it's path 'Path' usually, but this is not guaranteed.
+  if (process.platform === 'win32') {
+    PATH = 'Path'
     Object.keys(process.env).forEach(function (e) {
       if (e.match(/^PATH$/i)) {
         PATH = e
@@ -177,10 +187,9 @@ function getPATHKey() {
 /**
  * Get $PATH separator based on environment
  */
-function getPATHSeparator() {
-  return process.platform === "win32" ? ";" : ":"
+function getPATHSeparator () {
+  return process.platform === 'win32' ? ';' : ':'
 }
-
 
 module.exports = setPathAsync
 module.exports.get = getPathAsync
--- test/index.js
+++ test/index.js
@@ -1,168 +1,167 @@
-"use strict"
+'use strict'
 
-var test = require('tape')
+const test = require('tape')
 
-var path = require('path')
-var fs = require('fs')
-var os = require('os')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
 
-var which = require('which')
+const which = require('which')
 
-var npmPath = require('../')
+const npmPath = require('../')
 
-var SEP = npmPath.SEPARATOR
-var PATH = npmPath.PATH
+const SEP = npmPath.SEPARATOR
+const PATH = npmPath.PATH
 
-var level0 = path.join(__dirname, 'fixture', 'level0')
-var level1 = path.join(level0, 'node_modules', 'level1')
-var level2 = path.join(level1, 'node_modules', 'level2')
+const level0 = path.join(__dirname, 'fixture', 'level0')
+const level1 = path.join(level0, 'node_modules', 'level1')
+const level2 = path.join(level1, 'node_modules', 'level2')
 
-var level = [level0, level1, level2]
-var binPath = level.map(function(levelPath) {
-  return path.join(levelPath, "node_modules", ".bin")
+const level = [level0, level1, level2]
+const binPath = level.map(function (levelPath) {
+  return path.join(levelPath, 'node_modules', '.bin')
 })
 
-test('exports separator', function(t) {
+test('exports separator', function (t) {
   t.ok(npmPath.SEPARATOR)
   t.end()
 })
 
-test('exports $PATH key', function(t) {
+test('exports $PATH key', function (t) {
   t.ok(npmPath.PATH)
   t.end()
 })
 
-test('includes current node executable dir', function(t) {
-  var level0Path = npmPath.getSync({cwd: level0})
-  t.ok(level0Path.indexOf(path.dirname(process.execPath) + SEP) != -1)
+test('includes current node executable dir', function (t) {
+  const level0Path = npmPath.getSync({cwd: level0})
+  t.ok(level0Path.indexOf(path.dirname(process.execPath) + SEP) !== -1)
   t.end()
 })
 
-test('async version works', function(t) {
-  var isAsync = false
-  npmPath.get({cwd: level0}, function(err, level0Path) {
+test('async version works', function (t) {
+  let isAsync = false
+  npmPath.get({cwd: level0}, function (err, level0Path) {
     t.ifError(err)
     t.ok(isAsync)
-    t.ok(level0Path.indexOf(path.dirname(process.execPath) + SEP) != -1)
+    t.ok(level0Path.indexOf(path.dirname(process.execPath) + SEP) !== -1)
     t.end()
   })
   isAsync = true // can only be set if above callback not synchronous
 })
 
-test('no fn == sync', function(t) {
-  var level0Path = npmPath.get({cwd: level0})
-  t.ok(level0Path.indexOf(path.dirname(process.execPath) + SEP) != -1)
+test('no fn == sync', function (t) {
+  const level0Path = npmPath.get({cwd: level0})
+  t.ok(level0Path.indexOf(path.dirname(process.execPath) + SEP) !== -1)
   t.end()
 })
 
-test('sync options is optional', function(t) {
-  var newPath = npmPath.get()
-  t.ok(newPath.indexOf(path.dirname(process.execPath) + SEP) != -1)
+test('sync options is optional', function (t) {
+  const newPath = npmPath.get()
+  t.ok(newPath.indexOf(path.dirname(process.execPath) + SEP) !== -1)
   t.end()
 })
 
-test('async options is optional', function(t) {
-  var isAsync = false
-  npmPath.get(function(err, newPath) {
+test('async options is optional', function (t) {
+  let isAsync = false
+  npmPath.get(function (err, newPath) {
     t.ifError(err)
-    t.ok(newPath.indexOf(path.dirname(process.execPath) + SEP) != -1)
+    t.ok(newPath.indexOf(path.dirname(process.execPath) + SEP) !== -1)
     t.ok(isAsync)
     t.end()
   })
   isAsync = true // can only be set if above callback not synchronous
 })
 
-test('includes bin from sibling dirs', function(t) {
-  t.test('from existing sibling directory', function(t) {
-    var level1Path = npmPath.getSync({cwd: path.join(level[0], 'test')})
-    t.ok(level1Path.indexOf(binPath[0] + SEP) != -1, 'should include level 0 .bin')
+test('includes bin from sibling dirs', function (t) {
+  t.test('from existing sibling directory', function (t) {
+    const level1Path = npmPath.getSync({cwd: path.join(level[0], 'test')})
+    t.ok(level1Path.indexOf(binPath[0] + SEP) !== -1, 'should include level 0 .bin')
     t.ok(level1Path.indexOf(binPath[2] + SEP) === -1, 'should not include child paths')
     t.end()
   })
 
-  t.test('from existing sibling directory async', function(t) {
-    npmPath({cwd: path.join(level[0], 'test')}, function(err, level1Path) {
+  t.test('from existing sibling directory async', function (t) {
+    npmPath({cwd: path.join(level[0], 'test')}, function (err, level1Path) {
       t.ifError(err)
-      t.ok(level1Path.indexOf(binPath[0] + SEP) != -1, 'should include level 0 .bin')
+      t.ok(level1Path.indexOf(binPath[0] + SEP) !== -1, 'should include level 0 .bin')
       t.ok(level1Path.indexOf(binPath[2] + SEP) === -1, 'should not include child paths')
       t.end()
     })
   })
 })
 
-test('includes all .bin dirs in all parent node_modules folders', function(t) {
-  t.test('no nesting', function(t) {
-    var level0Path = npmPath.getSync({cwd: level[0]})
-    t.ok(level0Path.indexOf(binPath[0] + SEP) != -1, 'should include level 0 .bin')
+test('includes all .bin dirs in all parent node_modules folders', function (t) {
+  t.test('no nesting', function (t) {
+    const level0Path = npmPath.getSync({cwd: level[0]})
+    t.ok(level0Path.indexOf(binPath[0] + SEP) !== -1, 'should include level 0 .bin')
     t.ok(level0Path.indexOf(binPath[1] + SEP) === -1, 'should not include child paths')
     t.ok(level0Path.indexOf(binPath[2] + SEP) === -1, 'should not include child paths')
     t.end()
   })
 
-  t.test('1 level of nesting', function(t) {
-    var level1Path = npmPath.getSync({cwd: level[1]})
-    t.ok(level1Path.indexOf(binPath[0] + SEP) != -1, 'should include level 0 .bin')
-    t.ok(level1Path.indexOf(binPath[1] + SEP) != -1, 'should include level 1 .bin')
+  t.test('1 level of nesting', function (t) {
+    const level1Path = npmPath.getSync({cwd: level[1]})
+    t.ok(level1Path.indexOf(binPath[0] + SEP) !== -1, 'should include level 0 .bin')
+    t.ok(level1Path.indexOf(binPath[1] + SEP) !== -1, 'should include level 1 .bin')
     t.ok(level1Path.indexOf(binPath[2] + SEP) === -1, 'should not include child paths')
     t.end()
   })
 
-  t.test('2 levels of nesting', function(t) {
-    var level1Path = npmPath.getSync({cwd: level[2]})
-    t.ok(level1Path.indexOf(binPath[0] + SEP) != -1, 'should include level 0 .bin')
-    t.ok(level1Path.indexOf(binPath[1] + SEP) != -1, 'should include level 1 .bin')
-    t.ok(level1Path.indexOf(binPath[2] + SEP) != -1, 'should include level 2 .bin')
+  t.test('2 levels of nesting', function (t) {
+    const level1Path = npmPath.getSync({cwd: level[2]})
+    t.ok(level1Path.indexOf(binPath[0] + SEP) !== -1, 'should include level 0 .bin')
+    t.ok(level1Path.indexOf(binPath[1] + SEP) !== -1, 'should include level 1 .bin')
+    t.ok(level1Path.indexOf(binPath[2] + SEP) !== -1, 'should include level 2 .bin')
     t.end()
   })
 
   t.end()
 })
 
-test('handles directories with node_modules in the name', function(t) {
-  var trickyL0 = level[0].replace('level0', 'level0_node_modules')
-  var trickyL1 = level[1].replace('level0', 'level0_node_modules')
-  var trickyL2 = level[2].replace('level0', 'level0_node_modules')
+test('handles directories with node_modules in the name', function (t) {
+  const trickyL0 = level[0].replace('level0', 'level0_node_modules')
+  const trickyL1 = level[1].replace('level0', 'level0_node_modules')
+  const trickyL2 = level[2].replace('level0', 'level0_node_modules')
 
-  t.test('no nesting', function(t) {
-    var level0Path = npmPath.getSync({cwd: trickyL0})
-    t.ok(level0Path.indexOf(path.join(trickyL0, 'node_modules', '.bin') + SEP) != -1, 'should include level 0 .bin')
+  t.test('no nesting', function (t) {
+    const level0Path = npmPath.getSync({cwd: trickyL0})
+    t.ok(level0Path.indexOf(path.join(trickyL0, 'node_modules', '.bin') + SEP) !== -1, 'should include level 0 .bin')
     t.end()
   })
 
-  t.test('1 level of nesting', function(t) {
-    var level1Path = npmPath.getSync({cwd: trickyL1})
+  t.test('1 level of nesting', function (t) {
+    const level1Path = npmPath.getSync({cwd: trickyL1})
 
-    t.ok(level1Path.indexOf(path.join(trickyL0, 'node_modules', '.bin') + SEP) != -1, 'should include level 0 .bin')
-    t.ok(level1Path.indexOf(path.join(trickyL1, 'node_modules', '.bin') + SEP) != -1, 'should include level 1 .bin')
+    t.ok(level1Path.indexOf(path.join(trickyL0, 'node_modules', '.bin') + SEP) !== -1, 'should include level 0 .bin')
+    t.ok(level1Path.indexOf(path.join(trickyL1, 'node_modules', '.bin') + SEP) !== -1, 'should include level 1 .bin')
     t.end()
   })
 
-  t.test('2 levels of nesting', function(t) {
-    var level2Path = npmPath.getSync({cwd: trickyL2})
+  t.test('2 levels of nesting', function (t) {
+    const level2Path = npmPath.getSync({cwd: trickyL2})
 
-    t.ok(level2Path.indexOf(path.join(trickyL0, 'node_modules', '.bin') + SEP) != -1, 'should include level 0 .bin')
-    t.ok(level2Path.indexOf(path.join(trickyL1, 'node_modules', '.bin') + SEP) != -1, 'should include level 1 .bin')
-    t.ok(level2Path.indexOf(path.join(trickyL2, 'node_modules', '.bin') + SEP) != -1, 'should include level 1 .bin')
+    t.ok(level2Path.indexOf(path.join(trickyL0, 'node_modules', '.bin') + SEP) !== -1, 'should include level 0 .bin')
+    t.ok(level2Path.indexOf(path.join(trickyL1, 'node_modules', '.bin') + SEP) !== -1, 'should include level 1 .bin')
+    t.ok(level2Path.indexOf(path.join(trickyL2, 'node_modules', '.bin') + SEP) !== -1, 'should include level 1 .bin')
     t.end()
   })
 
   t.end()
 })
 
-test('can set path', function(t) {
-  var oldPath = process.env[PATH]
+test('can set path', function (t) {
+  const oldPath = process.env[PATH]
   npmPath.set.sync()
-  var newPath = process.env[PATH]
+  const newPath = process.env[PATH]
   t.notDeepEqual(oldPath, newPath)
   process.env[PATH] = oldPath
   t.end()
 })
 
-test('includes node-gyp bundled with current npm', function(t) {
-  var oldPath = process.env[PATH]
-  var oldGypPath = which.sync('node-gyp')
+test('includes node-gyp bundled with current npm', function (t) {
+  const oldPath = process.env[PATH]
   npmPath()
-  var newGypPath = which.sync('node-gyp')
+  const newGypPath = which.sync('node-gyp')
   t.ok(newGypPath)
   t.ok(fs.existsSync(newGypPath))
   t.ok(newGypPath.indexOf(path.join('npm', 'bin', 'node-gyp-bin') + SEP !== -1))
@@ -170,10 +169,10 @@ test('includes node-gyp bundled with current npm', function(t) {
   t.end()
 })
 
-test('remove duplicated entries', function(t) {
+test('remove duplicated entries', function (t) {
   const pathWithDuplicates = '/bin:/sbin:/bin'
 
-  var newPath = npmPath({env:{PATH:pathWithDuplicates}}).split(':')
+  const newPath = npmPath({env: {PATH: pathWithDuplicates}}).split(':')
 
   // Set size is equal to array length if there are no duplicates
   t.equal(newPath.length, (new Set(newPath)).size)
@@ -181,18 +180,23 @@ test('remove duplicated entries', function(t) {
   t.end()
 })
 
-test('can set path to npm root to use for node-gyp lookup', function(t) {
-  var oldPath = process.env[PATH]
-  var pathToNpm = path.resolve(
+test('can set path to npm root to use for node-gyp lookup', function (t) {
+  const oldPath = process.env[PATH]
+  const pathToNpm = path.resolve(
     fs.realpathSync(which.sync('npm')),
     '..',
     '..'
   )
 
-  var tmpFile = path.join(os.tmpdir(), 'npm-path-custom-npm')
-  try {fs.unlinkSync(tmpFile)}catch(e){}
+  const tmpFile = path.join(os.tmpdir(), 'npm-path-custom-npm')
+  try {
+    fs.unlinkSync(tmpFile)
+  } catch (err) {
+    err // ignore
+  }
+
   fs.linkSync(pathToNpm, tmpFile)
-  var newPath = npmPath.get({
+  const newPath = npmPath.get({
     npm: tmpFile
   })
   t.ok(newPath.indexOf(
